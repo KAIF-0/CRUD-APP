cf8ee847663bcff2319135ecca60520ad37bddd5
KAIF 0 <kaif8700979251@gmail.com>
2024-04-07 20:02:40 +0530

    implementing new concept

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 
 let addBtn = document.getElementById('addbtn')
-addBtn.addEventListener('click', addChapter)
-
-
-let parentList = document.getElementById("parentList")
-
-function addChapter(e){
-
+addBtn.addEventListener('click', (e)=>{
     if (parentList.children[0].className == "d-flex justify-content-center"){
         parentList.children[0].remove()
     }
@@ -23,7 +17,42 @@ newLi.innerHTML = `<h3 class="flex-grow-1">${newLi.textContent = currentInput.va
 
 parentList.appendChild(newLi)
 
-}
+},
+
+function removeChapter(e){
+    e.parentElement.remove()
+
+    let parentList = document.getElementById("parentList")
+    if(parentList.children.length <=0){
+        let emptyMsg = document.createElement("h3") 
+        emptyMsg.textContent = "Nothinf is here."
+        emptyMsg.className = "d-flex justify-content-center";
+        parentList.appendChild(emptyMsg)        
+    }
+})
+
+
+let parentList = document.getElementById("parentList")
+
+// function addChapter(e){
+
+//     if (parentList.children[0].className == "d-flex justify-content-center"){
+//         parentList.children[0].remove()
+//     }
+    
+// let currentInput = e.currentTarget.previousElementSibling
+
+// let newLi = document.createElement("li")
+// // newLi.classList.add("list-group-item")
+//                     //OR
+// newLi.className = "list-group-item d-flex justify-content-between";
+// newLi.innerHTML = `<h3 class="flex-grow-1">${newLi.textContent = currentInput.value}</h3>
+//                 <button id="edit" class="btn btn-warning mx-3" onclick="editChapter(this)">EDIT</button>
+//                 <button id="remove" class="btn btn-danger" onclick="removeChapter(this)">REMOVE</button></li>`
+
+// parentList.appendChild(newLi)
+
+// }
 
 function removeChapter(e){
     e.parentElement.remove()
